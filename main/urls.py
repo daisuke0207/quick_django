20ed90fe5db013c63f9759bca75dd1fcd8e80d5a
daisuke0207 <daisuke02074869@gmail.com>
2021-01-13 08:49:30 +0900

    iftag ルーティング追加

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('temp', views.temp, name='temp'),
   path('list', views.list, name='list'),
+  path('iftag', views.iftag, name='iftag'),
 ]
