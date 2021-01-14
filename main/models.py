d002b99cf588b8ed5e8917a15b57d70fa033d305
daisuke0207 <daisuke02074869@gmail.com>
2021-01-14 17:24:10 +0900

    reviewモデル作成

--- main/models.py
+++ main/models.py
@@ -31,3 +31,19 @@ class Book(models.Model):
     # モデルの文字列表現
     def __str__(self):
       return f'{self.title}({self.publisher}/{self.price}円)'
+
+
+class Review(models.Model):
+    # 書籍情報との関連付け
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    name = models.CharField(
+        verbose_name='名前',
+        max_length=20
+    )
+    body = models.TextField(
+      verbose_name='本文',
+      max_length=255
+    )
+    # レビュー情報の文字列表現
+    def __str__(self):
+        return f'{self.name}: {self.body[:10]}'
