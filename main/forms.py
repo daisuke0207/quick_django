562a3736e9d401a6e19d8dad4e1f70adfa5708ff
daisuke0207 <daisuke02074869@gmail.com>
2021-01-15 08:45:16 +0900

    Create forms.py

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+# フォームを定義
+class BookForm(forms.Form):
+    # 個々の項目を定義
+    isbn = forms.CharField(label='ISBNコード', required=True, max_length=20)
+    title = forms.CharField(label='書名', required=True, max_length=100)
+    price = forms.IntegerField(label='価格', required=True, min_value=0)
+    publisher = forms.ChoiceField(label='出版社',
+        choices = [
+            ('翔泳社', '翔泳社'),
+            ('技術評論社', '技術評論社'),
+            ('秀和システム', '秀和システム'),
+            ('SBクリエイティブ', 'SBクリエイティブ'),
+            ('日経BP', '日経BP'),
+        ])
+    published = forms.DateField(label='刊行日', required=True)
